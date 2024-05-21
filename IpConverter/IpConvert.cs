4c0aef0cf01478b323e70de16156603ed75dc3a5
Adron Hall <adron@thrashingcode.com>
2024-05-20 23:01:28 -0700

    Refactored with a boom!

--- IpConverter/IpConvert.cs
+++ IpConverter/IpConvert.cs
@@ -4,14 +4,8 @@ public static class IpConvert
 {
     public static uint ToInt32(string ip)
     {
-        string[] octets = ip.Split('.');
-        uint ipNumber = 0;
-
-        ipNumber += Convert.ToUInt32(octets[0]) << 24; // Shift the first octet left by 24 bits
-        ipNumber += Convert.ToUInt32(octets[1]) << 16; // Shift the second octet left by 16 bits
-        ipNumber += Convert.ToUInt32(octets[2]) << 8;  // Shift the third octet left by 8 bits
-        ipNumber += Convert.ToUInt32(octets[3]);       // Fourth octet stays as is
-
-        return ipNumber;
+        return ip.Split('.')
+            .Select(octet => Convert.ToUInt32(octet))
+            .Aggregate((result, octet) => (result << 8) + octet);
     }
 }
--- IpConverterTests/IpConverterTests.cs
+++ IpConverterTests/IpConverterTests.cs
@@ -4,14 +4,39 @@ namespace IpConverterTests;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
+    [Test]
+    public void TestIPtoInt_ValidIP()
     {
+        Assert.That(IpConvert.ToInt32("128.32.10.1"), Is.EqualTo(2149583361), "Incorrect answer for ip = \"128.32.10.1\"");
     }
 
     [Test]
-    public void TestIPtoInt()
+    public void TestIPtoInt_AllZeros()
     {
-        Assert.That(IpConvert.ToInt32("128.32.10.1"), Is.EqualTo(2149583361), "Incorrect answer for ip = \"128.32.10.1\"");
+        Assert.That(IpConvert.ToInt32("0.0.0.0"), Is.EqualTo(0), "Incorrect answer for ip = \"0.0.0.0\"");
+    }
+
+    [Test]
+    public void TestIPtoInt_AllOnes()
+    {
+        Assert.That(IpConvert.ToInt32("255.255.255.255"), Is.EqualTo(4294967295), "Incorrect answer for ip = \"255.255.255.255\"");
+    }
+
+    [Test]
+    public void TestIPtoInt_LeadingZeros()
+    {
+        Assert.That(IpConvert.ToInt32("001.002.003.004"), Is.EqualTo(16909060), "Incorrect answer for ip = \"001.002.003.004\"");
+    }
+
+    [Test]
+    public void TestIPtoInt_RandomIP()
+    {
+        Assert.That(IpConvert.ToInt32("192.168.0.1"), Is.EqualTo(3232235521), "Incorrect answer for ip = \"192.168.0.1\"");
+    }
+
+    [Test]
+    public void TestIPtoInt_RandomIP2()
+    {
+        Assert.That(IpConvert.ToInt32("10.0.0.1"), Is.EqualTo(167772161), "Incorrect answer for ip = \"10.0.0.1\"");
     }
 }
